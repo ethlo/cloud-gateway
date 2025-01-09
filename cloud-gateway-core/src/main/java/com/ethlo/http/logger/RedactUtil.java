b0c6ea53e0dc3db6f85e36d21cca0b593563c78e
Morten Haraldsen <dev@ethlo.com>
2025-01-09 12:36:06 +0100

    Make

--- cloud-gateway-core/src/main/java/com/ethlo/http/logger/RedactUtil.java
+++ cloud-gateway-core/src/main/java/com/ethlo/http/logger/RedactUtil.java
@@ -9,16 +9,21 @@ public class RedactUtil
         return headerValues.stream().map(RedactUtil::redact).toList();
     }
 
-    public static String redact(final String input)
+    public static String redact(final String input, int include)
     {
         if (input == null)
         {
             return null;
         }
-        else if (input.length() <= 5)
+        else if (include < 0 || input.length() <= include + 6)
         {
             return "*****";
         }
-        return input.charAt(0) + "*****" + input.charAt(input.length() - 1);
+        return input.substring(0, include) + "*****" + input.substring(input.length() - include);
+    }
+
+    public static String redact(String input)
+    {
+        return redact(input, 1);
     }
 }
